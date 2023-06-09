0e3b2ed3c78b3236751b45db1b94e5da943383b1
Guntsv <guntsvzz@gmail.com>
2023-06-09 13:07:14 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
     <div>
       <h1 className = 'text-5xl font-bold'> Hello World</h1>
       <Link className = 'bg-sky-500 text-white border rounded border-sky-500 pt-2 pb-2 px-4 py-4' href = '/about'> About </Link>
+      
       <Button variant="contained">Contained Button</Button>
     </div>
 
