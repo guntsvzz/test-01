bcfdd32dfb18e63872faf68e82cd7413dc551d6d
Guntsv <guntsvzz@gmail.com>
2023-06-09 12:01:22 +0700

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 export default function About(){
     return(
-        <h1 className = 'text-5xl font-bold'>This is about page</h1>
+        <div>
+            <h1 className = 'text-5xl font-bold'>This is about page</h1>
+            <Link className = 'bg-sky-500 text-white border rounded border-sky-500 pt-2 pb-2 px-4 py-4' href = '/'> Back to main </Link>
+        </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+import Button from '@mui/material/Button';
+
 export default function Home() {
   return (
     <div>
       <h1 className = 'text-5xl font-bold'> Hello World</h1>
-      <Link className = 'bg-sky-500 text-white border rounded border-sky-500 pt-2 pb-2 px-4 py-4' href = {'/about'}> About </Link>
+      <Link className = 'bg-sky-500 text-white border rounded border-sky-500 pt-2 pb-2 px-4 py-4' href = '/about'> About </Link>
+      <Button variant="contained">Contained Button</Button>
     </div>
 
   )
