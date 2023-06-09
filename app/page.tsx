ab7ff2b30155ba466d905c764908c255e2062e29
Guntsv <guntsvzz@gmail.com>
2023-06-09 11:48:05 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
-
+import Link from 'next/link'
 export default function Home() {
   return (
-    <h1 className = 'text-5xl font-bold'>Hello World</h1>
+    <div>
+      <h1 className = 'text-5xl font-bold'> Hello World</h1>
+      <Link className = 'bg-sky-500 text-white border rounded border-sky-500 pt-2 pb-2 px-4 py-4' href = {'/about'}> About </Link>
+    </div>
+
   )
 }
